e89d91865db17a8fbdee1cef31e538afeab681a0
Eryk <eryk@helenowski.com>
2016-12-27 00:18:19 -0600

    Updated roll a ball

--- Roll a Ball/Assets/Scripts/PlayerController.cs
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -8,8 +8,11 @@ public class PlayerController : MonoBehaviour {
 	private Rigidbody rb;
 	public float speed;
 	private int count; 
+	private bool allowJump;
+	public float speedBoost;
 	public Text countText; 
 	public Text winText;
+	public Text actionText;
 
 	// Use this for initialization
 	void Start () {
@@ -17,21 +20,25 @@ public class PlayerController : MonoBehaviour {
 		count = 0;
 		SetCountText (); 
 		winText.text = "";
+		actionText.text = " ";
+		allowJump = true;
 	}
 	
 	// Update is called once per frame, before the frame is rendered
 	void Update () {
-		
 	} 
 
 	// FixedUpdate is called before any physics calculation, where all physics code goes
 	void FixedUpdate() {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
-
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical); 
-
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		rb.AddForce (movement * speed);
+
+		if (Input.GetKey (KeyCode.Space) && allowJump) {
+			rb.AddForce (new Vector3 (0, 250, 0));
+			allowJump = false;
+		}
 	}
 
 	//Called when our unity object first touches a trigger collider
@@ -41,14 +48,41 @@ public class PlayerController : MonoBehaviour {
 			other.gameObject.SetActive (false);
 			count++;
 			SetCountText ();
+		} else if (other.gameObject.CompareTag ("Speed Up")) {
+			StartCoroutine (DisplaySpeedBoost ());
+			rb.velocity *= speedBoost;
+			allowJump = true;
+		} else if (other.gameObject.CompareTag ("Stop Pill")) {
+			rb.velocity = Vector3.zero;
+			rb.angularVelocity = Vector3.zero;
+			transform.rotation = Quaternion.identity;
+			other.gameObject.SetActive (false);
+		} else if (other.gameObject.CompareTag ("Warp")) {
+			transform.position = new Vector3 (-transform.position.x, transform.position.y, -transform.position.z);
+		}
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.CompareTag("Ground")){
+			allowJump = true;
+		}
+	}
+
+	private IEnumerator DisplaySpeedBoost(){
+		lock (actionText) {
+			actionText.text = "Speed Boost!";
+			yield return new WaitForSeconds (2.5f);
+			actionText.text = "";
 		}
 	}
 
 	void SetCountText()
 	{
 		countText.text = "Count: " + count.ToString ();
-		if (count >= 12) {
+		if (count >= 17) {
 			winText.text = "You Win!"; 
+			gameObject.SetActive (false);
 		}
 	} 
 }
