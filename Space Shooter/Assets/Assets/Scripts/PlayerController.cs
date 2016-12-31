63dcfe86646bc04548ccb8831c30b4ac59593fc5
Eryk <eryk@helenowski.com>
2016-12-31 14:22:12 -0600

    Add Space Shooter

---
+++ Space Shooter/Assets/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary {
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+	private Rigidbody rb;
+	public float speed;
+	public float tilt;
+	public Boundary boundary;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		rb.velocity = new Vector3 (moveHorizontal, 0f, moveVertical) * speed;
+
+		rb.position = new Vector3 (Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax) , 
+								   0f, 
+								   Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax));
+		rb.rotation = Quaternion.Euler(0f, 0f, rb.velocity.x * -tilt);
+	}
+}
